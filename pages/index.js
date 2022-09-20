5a1e3723a615e82bfd734bea00528f7e8f3992d6
_dev_goyal__ <65810521+dgbkn@users.noreply.github.com>
2022-09-20 16:16:37 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,11 +14,11 @@ export default function Home() {
 
       <SearchComponent />
 
-</br></br></br>
+<br/><br/><br/>
 <a href="https://apkfab.com/mx-player-pro/com.mxtech.videoplayer.pro/apk?h=0ef4048b1ef9e836e0d0aa54446ba41a377b72d26a804f5fa5af6da4eae328a1">
 <Button colorScheme='blue'>MX Player PRO APK</Button>
 </a>
-</br>
+<br/>
 <a href="https://apkfab.com/nplayer/com.newin.nplayer.pro/apk?h=8f5b73317363085f6c1b156a6dd9bc0eb5be07e580a295ed5333710bb862aae8">
 <Button colorScheme='green'>N Player APK</Button>
 </a>
