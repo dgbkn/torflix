93dd2372810db2dcba8f7c9082e75c654c025ad5
_dev_goyal__ <65810521+dgbkn@users.noreply.github.com>
2023-03-19 12:05:07 +0530

    dev

--- pages/_app.js
+++ pages/_app.js
@@ -9,11 +9,18 @@ import "./plyr.css";
 
 
 function MyApp({ Component, pageProps }) {
-   return <ChakraProvider theme={theme}>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1402284079761185"
-     crossorigin="anonymous"></script>
+  return <ChakraProvider theme={theme}>
+    {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1402284079761185"
+      crossorigin="anonymous"></script> */}
+    <Script
+      id="Adsense-id"
+      data-ad-client="ca-pub-1402284079761185"
+      async="true"
+      strategy="beforeInteractive"
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1402284079761185"
+    />
     <Navbar />
-  <Component {...pageProps} />
+    <Component {...pageProps} />
   </ChakraProvider>
 
 }
