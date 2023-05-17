0ceb82db4fb1d8cbe543edb60c84a589fb74d67b
dgbkn <anandrambkn@gmail.com>
2023-05-17 05:59:49 +0530

    added stream mp4

--- pages/seeAllFiles.js
+++ pages/seeAllFiles.js
@@ -114,7 +114,8 @@ export default function SeeAllFiles() {
 
                                                 {(ele.name.includes(".mkv") || ele.name.includes(".mp4")) && <>
 
-                                                   <video height={'200px'} src={`https://seedr.torrentdev.workers.dev/downloadProxy?id=${ele.id}`} key={'video34p'} id={'plyrvid'} controls={true} style={{margin:15}}>
+                                                   <video height={'200px'}  key={'video34p'} id={'plyrvid'} controls={true} style={{margin:15}}>
+                                                   <source src={`https://seedr.torrentdev.workers.dev/downloadProxy?id=${ele.id}`} type='video/mp4' />
                                                     </video>
 
                                                     
